97f2e2c37c7f4df678f672f8f2de09165b781379
rkdd51 <rahuldubay51@gmail.com>
2021-06-17 20:23:56 +0530

    deeptaught

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import List from './components/List'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'font-awesome/css/font-awesome.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <List />
+    </div>
+  );
+}
+
+export default App;
